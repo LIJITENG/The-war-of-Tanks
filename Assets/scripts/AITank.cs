e54a18cda1dd33cc1f49978002e0787e1d3aa815
LIJITENG <372128585@qq.com>
2017-03-12 21:06:10 +0800

    AI寻路系统

--- Assets/scripts/AITank.cs
+++ Assets/scripts/AITank.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AITank : Unit {
     public GameObject player;
@@ -10,12 +11,14 @@ public class AITank : Unit {
 
     private float timer;
     private TankWeapon tw;
+    private NavMeshAgent nam;
 
     void Start()
     {
+        nam = GetComponent<NavMeshAgent>();
         tw = GetComponent<TankWeapon>();   
     }
-
+    /*
     void FixedUpdate()
     {
         timer += Time.fixedDeltaTime;//计时器 攻击冷却
@@ -34,7 +37,36 @@ public class AITank : Unit {
             tw.Shoot();//开炮
             timer = 0f;
         }
+        */
+
+   void FixedUpdate()
+    {
+        
+        timer += Time.fixedDeltaTime;//计时器 攻击冷却
+
+        if (player == null) return;//玩家死亡，就不移动 转向 开炮了
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist > attackRange)
+        {
+            nam.SetDestination(player.transform.position);// 移动
+           
+        }
+        else
+        {
+            nam.ResetPath();
+            transform.LookAt(player.transform.position);//转向
+            if (timer > shootCoolDown)
+            {
+                tw.Shoot();//开炮
+                timer = 0f;
+            }
+        }
+
+        
+
+        
     }
-
-
 }
+
+
+
